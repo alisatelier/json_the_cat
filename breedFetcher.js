7f111dfa2e0af9bd09bffeaf3c48ee24eb611c4b
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-07-14 19:24:41 -0600

    Created function with API integration to fetch the description of different breeds of cat.

---
+++ breedFetcher.js
@@ -0,0 +1,41 @@
+const needle = require('needle');
+
+const args = process.argv.slice(2);
+const breedName = args[0];
+
+const fetchBreedDescription = (breedName, callback) => {
+  const apiCall = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  needle.get(apiCall, (error, response) => {
+    if (error) {
+      callback(`Request failed ${error}`, null);
+      return;
+    }
+
+    const data = response.body;
+
+    if (data.length === 0) {
+      callback(`Breed "${breedName}" not found.`, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(`❌ API returned status code ${response.statusCode}`, null);
+      return;
+    }
+
+    const breed = data[0];
+    callback(null, breed.description);
+  });
+};
+
+const handleFetchResults = (error, data) => {
+  if (error) {
+    console.log("Error:", error);
+  } else {
+    console.log(`${breedName}: `, data);
+  }
+};
+
+fetchBreedDescription(breedName, handleFetchResults);
+
